a920b40b39a9ecadc17a10f12a4731d631bc3d8a
Mateus Arenas <mateusarenas97@gmail.com>
2024-06-10 20:59:22 -0300

    ...

--- api/index.php
+++ api/index.php
@@ -1,21 +1,23 @@
 <?php
     session_start(); 
+
+    echo 'olá mundo';
     
-    if(@$_REQUEST['base64'])
-    {
-        $redirect = urldecode(@$_GET['redirect']);
-        $redirect = base64_decode($redirect);
-    }
-    else
-    {
-        $redirect = urldecode(@$_GET['redirect']);
-    }
+    // if(@$_REQUEST['base64'])
+    // {
+    //     $redirect = urldecode(@$_GET['redirect']);
+    //     $redirect = base64_decode($redirect);
+    // }
+    // else
+    // {
+    //     $redirect = urldecode(@$_GET['redirect']);
+    // }
 
-    if(isset($_SESSION['MSLogin'])){ 
-        if($redirect) header("location:{$redirect}"); 
-        else header('location:dashboard2.php?Pagina=RelatorioDiario'); 
-        exit; 
-    }
+    // if(isset($_SESSION['MSLogin'])){ 
+    //     if($redirect) header("location:{$redirect}"); 
+    //     else header('location:dashboard2.php?Pagina=RelatorioDiario'); 
+    //     exit; 
+    // }
 
-    header('location: login.php'); 
+    // header('location: login.php'); 
 ?>
