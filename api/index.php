815590a052b20095ba2d821ad0fd72838a53c9a9
Mateus Arenas <mateusarenas97@gmail.com>
2024-06-10 21:10:00 -0300

    ver

--- api/index.php
+++ api/index.php
@@ -1,26 +1,4 @@
 <?php
-header('content-type: application/json');
-// Configuração do cabeçalho Content-Disposition
-header('Content-Disposition: inline');
 
-session_start(); 
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
-    
-    // if(@$_REQUEST['base64'])
-    // {
-    //     $redirect = urldecode(@$_GET['redirect']);
-    //     $redirect = base64_decode($redirect);
-    // }
-    // else
-    // {
-    //     $redirect = urldecode(@$_GET['redirect']);
-    // }
-
-    // if(isset($_SESSION['MSLogin'])){ 
-    //     if($redirect) header("location:{$redirect}"); 
-    //     else header('location:dashboard2.php?Pagina=RelatorioDiario'); 
-    //     exit; 
-    // }
-
-    // header('location: login.php'); 
-?>
+// Forward Vercel requests to normal index.php
+require __DIR__ . '../index.php';
