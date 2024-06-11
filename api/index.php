985d88525b054853d2cd3ebcedfbac105871ee98
Mateus Arenas <mateusarenas97@gmail.com>
2024-06-10 21:40:10 -0300

    just

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
-require __DIR__ . '/../index.php'; 
+    @include_once(__DIR__ . '../index.php'); 
+    @include_once('../index.php'); 
+    @include_once('./index.php'); 
+    @include_once('./index.php'); 
+    @include_once('index.php'); 
 ?>
