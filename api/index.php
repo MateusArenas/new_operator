b6d10fb4a29f6ed6477c7d7c59c72afa2b1de07d
Mateus Arenas <mateusarenas97@gmail.com>
2024-06-10 21:27:39 -0300

    test

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php 
 // require __DIR__ . '../index.php'; 
+// echo 'mateus';
 
-echo 'mateus';
-
+header('content-type: application/json');
+echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
 ?>
